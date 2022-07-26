190177c9a8926dce13fb4683a59af80463b9e717
sithukaungset <sithukaungset27@gmail.com>
2022-07-27 01:41:56 +0900

    pbft functions

--- main.go
+++ main.go
@@ -14,6 +14,34 @@ func main() {
 }
 
 func NewServer(nodeID string) *Server {
-	// A new node is created according to the passed NodeID. The default view of the node is 1000000, and the node starts three processes
+	// A new node is created according to the passed NodeID. The default view of the node is 1000000, and the node starts three processes: dispatchMsg,
+	// alarmToDispatcher and resolveMsg
+	node := NewNode(nodeID)
+	//Start service
+	server := &Server{node.NodeTable[nodeID], node}
+
+	//Set route
+	server.setRoute()
+	return server
+
+	//1. Create a node according to the passed nodeID
+	//2. Create a server service
+	//3. Set route
+}
+
+
+// NewNode(node ID)
+func NewNode(nodeID string) *Node {
+	const viewID = 1000000000 // temporary.
+
+	node := &Node{
+		// Hard-coded for test
+		NodeID: nodeID,
+		NodeTable: map[string]string{
+			"Apple"
+
+		}
+	}
+
 
 }
